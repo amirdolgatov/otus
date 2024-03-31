e8cd3ab4f746dc49417f8876a4cb8027a5cb6bc0
Amir <amirdolgatov@gmail.com>
2024-04-01 00:30:15 +0300

    third commit

--- helloworld/main.cpp
+++ helloworld/main.cpp
@@ -7,7 +7,7 @@
 
 int main(int, char **) {
     std::cout << "Version: " << version() << std::endl;
-    std::cout << "Hello, world1!" << std::endl;
+    std::cout << "Hello, world!" << std::endl;
 
     return 0;
 }
